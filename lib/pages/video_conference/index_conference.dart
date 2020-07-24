e5c57010fcda5780613f97fdaa4dff49fb471671
Neto <48258819+netopaez97@users.noreply.github.com>
2020-07-24 16:01:39 -0500

    bug para ingresar a una conferencia arreglado

--- lib/pages/video_conference/index_conference.dart
+++ lib/pages/video_conference/index_conference.dart
@@ -15,19 +15,8 @@ class IndexPage extends StatefulWidget {
 }
 
 class IndexState extends State<IndexPage> {
-  /// create a channelController to retrieve text value
-  final _channelController = TextEditingController();
-
-  /// if channel textField is validated to have error
 
   ClientRole _role = ClientRole.Broadcaster;
-
-  @override
-  void dispose() {
-    // dispose input controller
-    _channelController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,58 +35,14 @@ class IndexState extends State<IndexPage> {
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Text(
-                  "Welcome to your personal meeting in Meal.",
+                  "Are you ready to meet with your friends?\n\nClick on below!",
                   style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
                 ),
               ),
               Divider(),
 
               IconAdd(onPressed: onJoin)
-              // Row(
-              //   children: <Widget>[
-              //     Expanded(
-              //         child: TextField(
-              //       enabled: true,
-              //       controller: _channelController,
-              //       decoration: InputDecoration(
-              //         errorText:
-              //             _validateError ? 'Channel name is mandatory' : null,
-              //         border: UnderlineInputBorder(
-              //           borderSide: BorderSide(width: 1),
-              //         ),
-              //         hintText: 'Channel name',
-              //       ),
-              //     ))
-              //   ],
-              // ),
-              // Column(
-              //   children: [
-              //     ListTile(
-              //       title: Text("Join as diner"),
-              //       leading: Radio(
-              //         value: ClientRole.Broadcaster,
-              //         groupValue: _role,
-              //         onChanged: (ClientRole value) {
-              //           setState(() {
-              //             _role = value;
-              //           });
-              //         },
-              //       ),
-              //     ),
-              //     ListTile(
-              //       title: Text("Join as viewer"),
-              //       leading: Radio(
-              //         value: ClientRole.Audience,
-              //         groupValue: _role,
-              //         onChanged: (ClientRole value) {
-              //           setState(() {
-              //             _role = value;
-              //           });
-              //         },
-              //       ),
-              //     )
-              //   ],
-              // ),
             ],
           ),
         ),
@@ -106,25 +51,22 @@ class IndexState extends State<IndexPage> {
   }
 
   Future<void> onJoin() async {
-    // update input validation
 
-    if (_channelController.text.isNotEmpty) {
-      // await for camera and mic permissions before pushing video page
-      await _handleCameraAndMic();
-      // push video page with given channel name
-      final prefs = new UserPreferences();
-      final channelName = prefs.channelName;
-      prefs.channelName = '';
-      await Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ConferencePage(
-            channelName: channelName,
-            role: _role,
-          ),
+    // await for camera and mic permissions before pushing video page
+    await _handleCameraAndMic();
+    // push video page with given channel name
+    final prefs = new UserPreferences();
+    final channelName = prefs.channelName;
+    prefs.channelName = '';
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ConferencePage(
+          channelName: channelName,
+          role: _role,
         ),
-      );
-    }
+      ),
+    );
   }
 
   Future<void> _handleCameraAndMic() async {
